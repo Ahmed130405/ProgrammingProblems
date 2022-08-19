0abd050fec97bfd11dc0afa8880ae0c65a6bfda4
Ahmed130405 <Ahmed.130405@gmail.com>
2022-08-19 15:54:59 +0300

    Describe how to find ans

--- ProjectEuler/problem003.py
+++ ProjectEuler/problem003.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     n=600851475143
     for i in range(int(m.sqrt(n)),0,-1):
         if n % i==0:
-            print(i) # => 1 means n prime
+            print(i) # choose second-to-last value
             n = i
             if n == 1:
                 break
